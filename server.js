114db7f0b1b9eb58d4a9d61286fad2e1ffa50887
Youssef <youssefhassanp@gmail.com>
2026-04-15 21:54:45 +0200

    feat: test v4

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(
   cors({
     origin: ["https://broker-frontend-pi.vercel.app", "http://localhost:5173"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: [
       "Content-Type",
       "Authorization",
